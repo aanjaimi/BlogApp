46031917ad37cd8714c4fd7049c567d746829ed4
Ayoub Anjaimi <aanjaimi@student.1337.ma>
2024-10-24 02:03:07 +0100

    chore: fix BlogAPI type

--- lib/blogs/slice/index.ts
+++ lib/blogs/slice/index.ts
@@ -1,10 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { Blog } from '@/types/blog'
 import { fetchBlogs } from '../createAsyncThunk'
 
 // Define a type for the slice state
 export interface BlogState {
     blogs: Blog[] | undefined
+}
+
+export interface BlogAPI {
+    id: string
+    title: string
+    content: string
+    createdAt: string
+    userId: string
 }
 
 // Define the initial state using that type
@@ -16,13 +24,16 @@ export const blogSlice = createSlice({
     name: 'blogList',
     initialState,
     reducers: {
-        setBlogs: (state, action) => {
+        setBlogs: (state, action: PayloadAction<Blog[]>) => {
             state.blogs = action.payload
         },
     },
     extraReducers: (builder) => {
-        builder.addCase(fetchBlogs.fulfilled, (state, action) => {
-            state.blogs = action.payload
+        builder.addCase(fetchBlogs.fulfilled, (state, action: PayloadAction<BlogAPI[]>) => {
+            state.blogs = action.payload.map(blog => ({
+                ...blog,
+                createdAt: new Date(blog.createdAt),
+            }))
         })
     },
 })
